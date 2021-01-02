197bacb54d4f96d5f7dd6ab613f2cc817644d329
surendra-uikey <73986301+surendra-uikey@users.noreply.github.com>
2021-01-02 11:19:22 +0530

    asd

---
+++ src/main/java/com/surendra/aws/AwsElasticBeanstalkExampleApplication.java
@@ -0,0 +1,13 @@
+package com.surendra.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsElasticBeanstalkExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsElasticBeanstalkExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/surendra/aws/AwsElasticBeanstalkExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.surendra.aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsElasticBeanstalkExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
